fe47900df4512dde2c0caf3e594bd7370878ce1c
Tomasz Stachowiak <tjstachowiak@gmail.com>
2026-01-06 02:45:17 +0200

    swift version bump

--- Sources/QueryStringEncoder/Utils.swift
+++ Sources/QueryStringEncoder/Utils.swift
@@ -4,7 +4,7 @@ let allowedCharacters = CharacterSet(
     charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~-._"
 )
 
-let iso8601DateFormatter = {
+nonisolated(unsafe) let iso8601DateFormatter = {
     let dateFormatter = ISO8601DateFormatter()
     dateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
     return dateFormatter
